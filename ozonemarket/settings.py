c9b097f4943a35590df061e022a72d56d5cb9fca
peterumimo <peterumimo2@gmail.com>
2021-09-17 11:03:37 +0100

    Security and Logout season

--- ozonemarket/settings.py
+++ ozonemarket/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from sys import int_info
+from typing import cast
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+xvaiki%p(r-bwxam_b796&w5*rh-8=%5p_5g1ky!h3w#u^ca'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default = True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
     
 ]
 
@@ -53,7 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
 
 ROOT_URLCONF = 'ozonemarket.urls'
 
@@ -144,11 +153,11 @@ MESSAGE_TAGS = {
 
 #SMTP CONFIGURATION
 
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_POST =587
-EMAIL_HOST_USER ='peterumimo2@gmail.com'
-EMAIL_HOST_PASSWORD ='1759@@#2021'
-EMAIL_USE_TLS =True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_POST = config('EMAIL_POST', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
